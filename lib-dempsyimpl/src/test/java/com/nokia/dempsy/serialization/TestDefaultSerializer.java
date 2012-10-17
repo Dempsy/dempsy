77409a1b4f71d9ba7a7ebcd7c77595607c5665b5
Jim Carroll <thecarrolls@jiminger.com>
2012-10-17 10:32:09 -0400

    A few more Kryo tests.

--- lib-dempsyimpl/src/test/java/com/nokia/dempsy/serialization/TestDefaultSerializer.java
+++ lib-dempsyimpl/src/test/java/com/nokia/dempsy/serialization/TestDefaultSerializer.java
@@ -21,6 +21,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 import com.nokia.dempsy.serialization.java.JavaSerializer;
@@ -89,6 +92,31 @@ public class TestDefaultSerializer
       public int getInt() { return i; }
       
       public MockClass getMockClass() { return mock; }
+      
+      public boolean equals(Object obj)
+      {
+         Mock2 o = (Mock2)obj;
+         return o.i == i && mock.equals(o.mock);
+      }
+   }
+   
+   public static class Mock3 extends Mock2
+   {
+      public int myI = -1;
+      
+      public Mock3() {}
+      
+      public Mock3(int i, MockClass mock)
+      {
+         super(i,mock);
+         this.myI = i;
+      }
+      
+      public boolean equals(Object obj)
+      {
+         Mock3 o = (Mock3)obj;
+         return super.equals(obj) && myI == o.myI;
+      }
    }
    
    @Test
@@ -100,6 +128,56 @@ public class TestDefaultSerializer
       Mock2 o2 = (Mock2)ser.deserialize(data);
       assertEquals(1,o2.getInt());
       assertEquals(new MockClass(2, "Hello"),o2.getMockClass());
+   }
+   
+   @Test
+   public void testChildClassSerialization() throws Throwable
+   {
+      KryoSerializer<Object> ser = new KryoSerializer<Object>();
+      Mock2 o = new Mock3(1, new MockClass(2, "Hello"));
+      byte[] data = ser.serialize(o);
+      Mock2 o2 = (Mock2)ser.deserialize(data);
+      assertEquals(1,o2.getInt());
+      assertEquals(new MockClass(2, "Hello"),o2.getMockClass());
+      assertTrue(o2 instanceof Mock3);
+      assertEquals(1,((Mock3)o2).myI);
+   }
+      
+   @Test
+   public void testChildClassSerializationWithRegistration() throws Throwable
+   {
+      KryoSerializer<Object> serR = new KryoSerializer<Object>(new Registration(MockClass.class.getName(),10));
+      KryoSerializer<Object> serRR = new KryoSerializer<Object>(new Registration(MockClass.class.getName(),10),new Registration(Mock3.class.getName(), 11));
+      KryoSerializer<Object> ser = new KryoSerializer<Object>();
+      Mock2 o = new Mock3(1, new MockClass(2, "Hello"));
+      byte[] data = ser.serialize(o);
+      byte[] dataR = serR.serialize(o);
+      byte[] dataRR = serRR.serialize(o);
+      assertTrue(dataR.length < data.length);
+      assertTrue(dataRR.length < dataR.length);
+      Mock2 o2 = (Mock2)ser.deserialize(data);
+      assertEquals(1,o2.getInt());
+      assertEquals(new MockClass(2, "Hello"),o2.getMockClass());
+      assertTrue(o2 instanceof Mock3);
+      assertEquals(1,((Mock3)o2).myI);
+   }
+   
+   @Test
+   public void testCollectionSerialization() throws Throwable
+   {
+      KryoSerializer<Object> ser = new KryoSerializer<Object>();
+      ArrayList<Mock2> mess = new ArrayList<Mock2>();
+      for (int i = 0; i < 10; i++)
+      {
+         if (i % 2 == 0)
+            mess.add(new Mock3(i,new MockClass(-i,"Hello:" + i)));
+         else
+            mess.add(new Mock2(i,new MockClass(-i,"Hello:" + i)));
+      }
+      byte[] data = ser.serialize(mess);
+      @SuppressWarnings("unchecked")
+      List<Mock2> des = (List<Mock2>)ser.deserialize(data);
+      assertEquals(mess,des);
    }
       
 }
