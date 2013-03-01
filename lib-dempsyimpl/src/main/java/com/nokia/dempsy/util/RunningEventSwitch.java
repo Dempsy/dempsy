9717bad0db57f4d722453fecd87ed20c29d9a1d8
Jim Carroll <thecarrolls@jiminger.com>
2013-03-01 15:26:24 -0500

    Add the open source license text to some of the new utilities that were missing it.

--- lib-dempsyimpl/src/main/java/com/nokia/dempsy/util/RunningEventSwitch.java
+++ lib-dempsyimpl/src/main/java/com/nokia/dempsy/util/RunningEventSwitch.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.nokia.dempsy.util;
 
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -26,8 +42,8 @@ public class RunningEventSwitch
    {
       // this is synchronized because it's used as a condition variable 
       // along with the condition.
-      synchronized(isRunning) { isRunning.set(true); }
       stopRunning.set(false);
+      isRunning.set(true);
       
       synchronized(runningGate)
       {
