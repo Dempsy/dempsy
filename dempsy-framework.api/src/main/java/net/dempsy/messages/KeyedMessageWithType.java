f5a6a33852fd749f5dbb670f5b05ace8012792f0
Ben Zenittini <bzenittini@ptc.com>
2017-06-13 09:55:20 -0400

    Added toString methods to KeyedMessage and KeyedMessageWithType.

--- dempsy-framework.api/src/main/java/net/dempsy/messages/KeyedMessageWithType.java
+++ dempsy-framework.api/src/main/java/net/dempsy/messages/KeyedMessageWithType.java
@@ -1,10 +1,21 @@
 package net.dempsy.messages;
 
+import java.util.Arrays;
+
 public class KeyedMessageWithType extends KeyedMessage {
     public final String[] messageTypes;
 
     public KeyedMessageWithType(final Object key, final Object message, final String... messageTypes) {
         super(key, message);
         this.messageTypes = messageTypes;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder()
+                .append("KeyedMessageWithType [key=").append(key)
+                .append(", message=").append(message)
+                .append(", messageTypes=").append(Arrays.toString(messageTypes))
+                .append("]").toString();
     }
 }
